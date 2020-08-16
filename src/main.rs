bcdc2b105ac8dbfb2245e99dacdad83fa90d0da2
enjhnsn2 <e5johnso@eng.ucsd.edu>
2020-08-16 17:08:53 -0500

    completed commandline parsing

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 pub mod lattices;
 pub mod utils;
 pub mod analyses;
-use utils::{get_cfgs};
-use std::env;
-use analyses::stack_analyzer::{analyze_stack};
+use utils::get_cfgs;
+use analyses::stack_analyzer::analyze_stack;
 use clap::{Arg, App};
 
+pub struct Config{
+    module_path: String,
+    num_jobs: u32,
+    output_path : String,
+    has_output : bool,
+    quiet : bool
+}
 
-fn run(binpath : &str){
+fn run(config : Config){
     // let cfgs = get_cfgs(binpath);
-    for cfg in get_cfgs(binpath).iter(){
+    for cfg in get_cfgs(&config.module_path).iter(){
         //TODO: check instruction legality
         println!("Checking Instruction Legality");
         //TODO: check stack safety
@@ -25,17 +31,9 @@ fn run(binpath : &str){
 }
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // // println!("{:?}", args);
-    // run(&args[1]);
     let matches = App::new("VeriWasm")
     .version("0.1.0")
     .about("Validates safety of native Wasm module")
-    // .arg(Arg::with_name("command")
-    //     .short("cmd")
-    //     .long("command")
-    //     .takes_value(true)
-    //     .help("A cool file"))
     .arg(Arg::with_name("module path")
         .short("i")
         .takes_value(true)
@@ -45,7 +43,7 @@ fn main() {
         .short("j")
         .long("jobs")
         .takes_value(true)
-        .help("Number of parallel threads"))
+        .help("Number of parallel threads (default 1)"))
     .arg(Arg::with_name("stats output path")
         .short("o")
         .long("output")
@@ -53,11 +51,26 @@ fn main() {
         .help("Path to output stats file"))
     .arg(Arg::with_name("quiet")
         .short("q")
-        .long("quiet")
-        .takes_value(true))
+        .long("quiet"))
     .get_matches();
 
-    run(matches.value_of("module path").unwrap());
+    let module_path = matches.value_of("module path").unwrap();
+    let num_jobs_opt = matches.value_of("jobs");
+    let output_path = matches.value_of("stats output path").unwrap_or("");
+    let num_jobs  = num_jobs_opt.map(|s| s.parse::<u32>().unwrap_or(1)).unwrap_or(1);
+    let quiet = matches.is_present("quiet");
+
+    let has_output = 
+    if output_path == "" {true} else {false};
+
+    let config = Config{
+        module_path: module_path.to_string(), 
+        num_jobs: num_jobs, 
+        output_path : output_path.to_string(),
+        has_output : has_output, 
+        quiet:quiet};
+
+    run(config);
 
 }
 
