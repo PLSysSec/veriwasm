0ad8f15523ed75423a03bd1172bb038ca0bc3b3a
Evan Johnson <e5johnso@eng.ucsd.edu>
2021-06-17 16:16:29 -0700

    start wasmtime support

---
+++ src/compilers/wasmtime.rs
@@ -0,0 +1,15 @@
+use yaxpeax_core::memory::repr::FileRepr;
+use yaxpeax_core::memory::reader;
+use yaxpeax_core::memory::repr::process::{
+    ELFExport, ELFImport, ELFSymbol, ModuleData, ModuleInfo,
+};
+
+// pub fn load_wasmtime_program(binpath: &str) -> ModuleData {
+//     let program = reader::load_from_path(Path::new(binpath)).unwrap();
+//     let program = if let FileRepr::Executable(program) = program {
+//         program
+//     } else {
+//         panic!("function:{} is not a valid path", binpath);
+//     };
+//     program
+// }
