860b51be9a21d9f0e1c368da4e1f10a2b0b14ccc
Evan Johnson <e5johnso@eng.ucsd.edu>
2021-06-23 15:56:12 -0700

    calling convention helpers

--- src/loaders/utils.rs
+++ src/loaders/utils.rs
@@ -1,6 +1,6 @@
 #![allow(non_camel_case_types)]
 
-use lucet_module::Signature;
+use lucet_module::{Signature, ValueType};
 use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
@@ -16,4 +16,46 @@ pub struct VwFuncInfo {
     pub signatures: Vec<Signature>,
     // Name -> Index
     pub indexes: HashMap<String, u32>,
+}
+
+// RDI, RSI, RDX, RCX, R8, R9,
+// 7,   6,   3,   2,   8,  9,    then stack slots
+pub enum ArgLoc {
+    StackSlot(u32, u32), //offset, size
+    Register(u32),       //register number
+}
+
+pub fn to_system_v(sig: &Signature) -> Vec<ArgLoc> {
+    let mut arg_locs = Vec::new();
+    let mut i_ctr = 0; // integer arg #
+    let mut f_ctr = 0; // floating point arg #
+    let mut stack_offset = 0;
+    for arg in &sig.params {
+        match arg {
+            ValueType::I32 | ValueType::I64 => {
+                match i_ctr {
+                    0 => arg_locs.push(ArgLoc::Register(7)),
+                    1 => arg_locs.push(ArgLoc::Register(6)),
+                    2 => arg_locs.push(ArgLoc::Register(3)),
+                    3 => arg_locs.push(ArgLoc::Register(2)),
+                    4 => arg_locs.push(ArgLoc::Register(8)),
+                    5 => arg_locs.push(ArgLoc::Register(9)),
+                    _ => {
+                        if let ValueType::I32 = arg {
+                            arg_locs.push(ArgLoc::StackSlot(stack_offset, 4));
+                            stack_offset += 4;
+                        } else {
+                            arg_locs.push(ArgLoc::StackSlot(stack_offset, 8));
+                            stack_offset += 8;
+                        }
+                    }
+                };
+                i_ctr += 1;
+            }
+            ValueType::F32 | ValueType::F64 => {
+                f_ctr += 1;
+            }
+        }
+    }
+    return arg_locs;
 }
